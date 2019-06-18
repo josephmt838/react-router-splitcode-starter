22e85fc01ee029e9a44729ffc8fe389f07217ff6
Joseph Thompson <joseph@Josephs-MacBook-Pro.local>
2019-06-18 19:41:59 -0400

    refactored spinner to be wrapped in a styled div

--- src/containers/Four0Four/index.js
+++ src/containers/Four0Four/index.js
@@ -2,9 +2,17 @@ import React from "react";
 
 const Four0Four = () => {
   return (
-    <div>
-      <h4>Page Not Found</h4>
-      <h1>404</h1>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column"
+      }}
+    >
+      <h4 style={{ fontSize: "24px" }}>Page Not Found</h4>
+      <h1 style={{ fontSize: "200px" }}>404</h1>
     </div>
   );
 };
--- src/helpers/dynamic-imports.js
+++ src/helpers/dynamic-imports.js
@@ -2,18 +2,35 @@ import React from "react";
 import DynamicImport from "../components/DynamicImport";
 import { Spinner } from "reactstrap";
 
+const WrappedSpinner = (Spin) => (
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh"
+    }}
+  >
+    <Spin
+      style={{ width: "200px", height: "200px" }}
+      type="grow"
+      color="primary"
+    />
+  </div>
+);
+
 const splits = {
   Four0Four: (props) => (
     <DynamicImport load={() => import("../containers/Four0Four")}>
       {(Component) =>
-        Component === null ? <Spinner /> : <Component {...props} />
+        Component === null ? WrappedSpinner(Spinner) : <Component {...props} />
       }
     </DynamicImport>
   ),
   Landing: (props) => (
     <DynamicImport load={() => import("../containers/Landing")}>
       {(Component) =>
-        Component === null ? <Spinner /> : <Component {...props} />
+        Component === null ? WrappedSpinner(Spinner) : <Component {...props} />
       }
     </DynamicImport>
   )
